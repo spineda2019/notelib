43dbac4fc1ba5fc6f73acc1e630ff9027ca46fe6
spineda2019 <spineda@wpi.edu>
2024-03-01 09:38:08 -0500

    show window, you can type now!

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
-#include <QApplication>
+#include <qapplication.h>
 
 #include "include/MainWindow.h"
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) {
+  QApplication notelib(argc, argv);
+  notelib.setApplicationName("NoteLib");
+  MainWindow main_window{};
+  main_window.setWindowTitle("Notelib");
+  main_window.show();
+
+  return notelib.exec();
+}
